299c5e6f4afb4d6b4a78458c3329bf109a9592eb
mookp12 <kraiviwat.p@gmail.com>
2025-08-01 16:07:37 +0700

    fix:logo link

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import logo from "./assets/logo.png"
+import logo from "/logo.png"
 import "./App.css";
 
 function NavBar() {
